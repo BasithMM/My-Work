e2b1c8076a876f7e28e283d7cc68b7355b32fca1
BasithMM <basithmm99@gmail.com>
2025-09-19 17:03:06 +0530

    index

--- script.js
+++ script.js
@@ -2,66 +2,66 @@ document.addEventListener('DOMContentLoaded', function() {
     const pinsContainer = document.getElementById('pinsContainer');
     const pinData = [
         {
-            id: 13,
+            id: 1,
             imageUrl: './image/joker.jpg',
             title: 'Joker'
         },
         {
-            id: 1,
+            id: 2,
             imageUrl: './image/ILM KHURASAN-1.png',
             title: 'Poster'
         },
         {
-            id: 2,
+            id: 3,
             imageUrl: './image/basith.jpeg',
             title: 'Design'
         },
         {
-            id: 3,
+            id: 4,
             imageUrl: './image/epoch zero.png',
             title: 'My site Logo'
         },
         {
-            id: 4,
+            id: 5,
             imageUrl: './image/jazora (1).jpg',
             title: 'business Logo'
         },
         {
-            id: 5,
+            id: 6,
             imageUrl: './image/logo for copy.jpg',
             title: 'Logo'
         },
         {
-            id: 6,
+            id: 7,
             imageUrl: './image/Paper Logo.jpg ',
             title: 'Logo'
         },
         {
-            id: 7,
+            id: 8,
             imageUrl: './image/GreenGlass.png',
             title: 'mockup Logo'
         },
         {
-            id: 8,
+            id: 9,
             imageUrl: './image/tea.jpg',
             title: 'poster'
         },
         {
-            id: 9,
+            id: 10,
             imageUrl: './image/child.jpg',
             title: 'poster'
         },
         {
-            id: 10,
+            id: 11,
             imageUrl: './image/father.jpg',
             title: 'poster'
         },
         {
-            id: 11,
+            id: 12,
             imageUrl: './image/picnic.jpg',
             title: 'poster'
         },{
-            id: 12,
+            id: 13,
             imageUrl: './image/speech.jpg',
             title: 'poster'
         },
